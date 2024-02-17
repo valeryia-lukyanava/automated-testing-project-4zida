dc2e49a7dcc0a23d7c0b7c31c8c51f7f7acd2bbd
ValeryiaLukyanava <lukyanava.valeryia@gmail.com>
2024-02-17 22:09:03 +0300

    added HOME_PAGE_TITLE

--- constants/titles.py
+++ constants/titles.py
@@ -3,3 +3,4 @@ from enum import Enum
 
 class Titles(str, Enum):
     HOME_PAGE_BROWSER_TITLE = "Pretraga Nekretnina za Prodaju i Izdavanje · 4zida"
+    HOME_PAGE_TITLE = "Pronađi idealnu nekretninu za sebe!"
