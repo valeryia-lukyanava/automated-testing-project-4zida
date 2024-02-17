b0451514ea3e0a4f9e008eb6b4d513f04746d271
ValeryiaLukyanava <lukyanava.valeryia@gmail.com>
2024-02-17 22:08:53 +0300

    added locators

--- constants/locators.py
+++ constants/locators.py
@@ -2,3 +2,5 @@ class HomePageLocators(str):
     META_ROBOTS = '//meta[@name="robots"]'
     LINK_CANONICAL = '//link[@rel="canonical"]'
     SEARCH_FORM = '//div[@test-data="search-form"]'
+    TITLE = '//h1'
+    FOOTER_LINKS = '//footer//a'
