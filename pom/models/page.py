cc5ece9ba75c5a92a741fef84e0521d6a33820a9
valeryia-lukyanava <lukyanava.valeryia@gmail.com>
2024-02-28 11:06:45 +0300

    tried without a delay

--- pom/models/page.py
+++ pom/models/page.py
@@ -104,7 +104,7 @@ class Page(PageInterface):
         """Navigate to the given URL"""
         normalized_url = url if url.startswith('http') else (self.config.base_url + url)
         logger.info("Page.visit() - Get URL: '%s'", normalized_url)
-        time.sleep(0.5)
+        # time.sleep(0.5)
         self.webdriver.get(normalized_url)
         logger.info(f"Client window width: {self._webdriver.execute_script(JS.CLIENT_WINDOW_WIDTH)}, window height: "
                     f"{self._webdriver.execute_script(JS.CLIENT_WINDOW_HEIGHT)}")
