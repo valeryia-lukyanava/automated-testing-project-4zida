21fefa3cfc7a4b903724713b70eaf9bccdd12e66
ValeryiaLukyanava <lukyanava.valeryia@gmail.com>
2024-02-29 11:32:25 +0300

    added locators

--- constants/locators.py
+++ constants/locators.py
@@ -1,7 +1,11 @@
+from constants.navigation_menu import NavigationMenu
+
+
 class HomePageLocators(str):
     HEADER_H1 = '//h1'
     HEADER_H2 = '//h2'
     HEADER_H3 = '//h3'
+    LOGO = '//header/a/img'
     HEADER_H3_QUICK_LINK = '//div[@test-data="premium-ads"]//h3'
     HEADER_H3_WIDGET = '//section[@test-data="blog-posts"]//h3'
     META_DESCRIPTION = '//meta[@name="description"]'
@@ -9,3 +13,9 @@ class HomePageLocators(str):
     LINK_CANONICAL = '//link[@rel="canonical"]'
     SEARCH_FORM = '//div[@test-data="search-form"]'
     FOOTER_LINKS = '//footer//a'
+    MAIN_MENU_BUTTON = '//header/button'
+    MENU_SALE = f'//header//nav//div[text()="{NavigationMenu.MENU_SALE}"]'
+    MENU_RENT = f'//header//nav//div[text()="{NavigationMenu.MENU_RENT}"]'
+    MENU_NEW = f'//header//nav//div[text()="{NavigationMenu.MENU_NEW}"]'
+    MENU_ADVERTISEMENT = f'//header//nav//div[text()="{NavigationMenu.MENU_ADVERTISEMENT}"]'
+    SUB_MENU = '//header//nav/a[text()='
